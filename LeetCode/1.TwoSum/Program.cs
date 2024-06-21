3870e0070e51ac1a81759b64fc6921af93ec9618
Ashish Patel <ashishpatel626@gmail.com>
2024-06-21 11:22:46 -0400

    Added Two LeetCode Problems
    Added TwoSum solution and setup AddTwoNumbers

---
+++ LeetCode/1.TwoSum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+  class Solution
+  {
+
+    public int[] TwoSum(int[] nums, int target)
+    {
+      IDictionary<int, int> dictionary = nums.Select((value, index) => new { value, index })
+                     .ToDictionary(key => key.index, value => value.value);
+
+      IDictionary<int, int> Seen = new Dictionary<int, int>();
+
+      List<int> result = new List<int>();
+
+      foreach (KeyValuePair<int, int> pair in dictionary)
+      {
+        int diff = target - pair.Value;
+
+        if (Seen.ContainsKey(diff))
+        {
+          result.Add(Seen[diff]);
+          result.Add(pair.Key);
+          break;
+        }
+        else if (!Seen.ContainsKey(pair.Value))
+        {
+          Seen.Add(pair.Value, pair.Key);
+        }
+
+      }
+      return result.ToArray();
+    }
+
+  }
+
+  class CallSolution
+  {
+    public static void Main(string[] args)
+    {
+      Solution Solution = new Solution();
+      Solution.TwoSum([3, 3], 6);
+    }
+  }
+}
+
---
+++ LeetCode/2.AddTwoNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AddTwoNumbers 
+{
+
+  public class Solution
+  {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+
+    }
+  }
+
+  class CallSolution
+  {
+    Solution Solution = new Solution();
+
+  }
+
+}
